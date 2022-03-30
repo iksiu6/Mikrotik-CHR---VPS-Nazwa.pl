e575982c1d2c36dff2885a8ad5034d6cba160b1f
iksiu6 <66210872+iksiu6@users.noreply.github.com>
2022-03-30 09:30:16 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,15 +2,9 @@
 wget https://download.mikrotik.com/routeros/6.49.2/chr-6.49.2.img.zip -O chr.img.zip  && \
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=512 chr.img /mnt && \
-
-
-
 #Check network adapter name in next line
 ADDRESS=`ip addr show enp1s0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 #Check network adapter name line before
-
-
-
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
@@ -19,15 +13,9 @@ echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where
  " > /mnt/rw/autorun.scr && \
 umount /mnt && \
 echo u > /proc/sysrq-trigger && \
-
-
-
 #Check drive name in next line
 dd if=chr.img bs=1024 of=/dev/sda && \
 #Check drive name in line before
-
-
-
 echo "sync disk" && \
 echo s > /proc/sysrq-trigger && \
 echo "Sleep 5 seconds" && \
