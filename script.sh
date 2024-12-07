618025e2203966128b120c565115897190aaed7d
iksiu6 <66210872+iksiu6@users.noreply.github.com>
2024-12-07 21:25:52 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ wget https://download.mikrotik.com/routeros/6.49.2/chr-6.49.2.img.zip -O chr.img
 gunzip -c chr.img.zip > chr.img  && \
 mount -o loop,offset=512 chr.img /mnt && \
 #Check network adapter name in next line
-ADDRESS=`ip addr show enp1s0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 #Check network adapter name line before
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
@@ -14,7 +14,7 @@ echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where
 umount /mnt && \
 echo u > /proc/sysrq-trigger && \
 #Check drive name in next line
-dd if=chr.img bs=1024 of=/dev/sda && \
+dd if=chr.img bs=1024 of=/dev/vda && \
 #Check drive name in line before
 echo "sync disk" && \
 echo s > /proc/sysrq-trigger && \
